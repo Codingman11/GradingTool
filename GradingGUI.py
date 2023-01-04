cf26d9bf91ded793cfa19925f982506e2bc37e4b
JessePel <jesse1.peltola@outlook.com>
2023-01-04 13:54:33 +0200

    Keskeneräinen

--- GradingGUI.py
+++ GradingGUI.py
@@ -4,14 +4,18 @@ import os
 import random
 import GradingGUI_library as gui
 
-dpg.create_context()
+# V0.1.0 students added to student window
+# V0.1.0.1 added errors to error window
 
+
+dpg.create_context()
 dpg.create_viewport(title="Testing", width=1200, height=600,
-                    resizable=True, always_on_top=True)
+                    resizable=False, always_on_top=True)
 dpg.configure_app(docking=True, docking_space=True,
                   load_init_file="custom_layout.ini")
 dpg.setup_dearpygui()
 
+studentList = []
 indent = 20
 left_window = dpg.generate_uuid()
 
@@ -23,18 +27,17 @@ student_list = dpg.generate_uuid()
 file_director = dpg.generate_uuid()
 error_list_window = dpg.generate_uuid()
 
-gui.initiate_problem_list()
 
 dpg.add_window(label="Opiskelijat", tag=left_window,
-                menubar=False, no_close=True, horizontal_scrollbar=True)
+                menubar=False, no_close=True, horizontal_scrollbar=True, autosize=True)
 
 dpg.add_window(label="Arviointitaulukko", tag=right_window,
-                menubar=False, no_close=True, no_move=True)
+                menubar=False, no_close=True, no_move=True, autosize=True, no_resize=True)
 
 dpg.add_window(label="Toiminnot", tag=bottom_window,
-                menubar=False, no_close=True, no_move=True)
-dpg.add_window(label="Virheet", tag=center_window,
                 menubar=False, no_close=True, no_move=True, autosize=True)
+dpg.add_window(label="Virheet", tag=center_window,
+                menubar=False, no_close=True, no_move=True, autosize=True, no_resize=True)
 
 dpg.add_group(tag=activities, horizontal=True,
                 parent=bottom_window, track_offset=0.5)
@@ -45,11 +48,10 @@ dpg.add_spacer(width=2, parent=activities)
 dpg.add_button(label="Kopioi palautekommentti leikepöydälle",
                 parent=activities)
 dpg.add_group(tag = student_list, parent=left_window, width=300)
-studentList = []
-# for i in range(25):
-#     studentList.append(random.randint(0, 100))
-dpg.add_listbox(items=[], parent=student_list, width=300, num_items=100, tag="Test")
-dpg.add_file_dialog(label="Valitse kansio", directory_selector=True, show=True, tag=file_director, width=700, height=400, callback=gui.dir_callback_exam, user_data=studentList)
+
+dpg.set_item_width(center_window, 100)
+dpg.add_listbox(items=[], parent=student_list, width=300, num_items=100, tag="Students")
+#dpg.add_file_dialog(label="Valitse kansio", directory_selector=True, show=True, tag=file_director, width=700, height=400, callback=gui.dir_callback_exam, user_data=studentList)
 
 category_texts = [
                 "toiminnallisuus tehtäväksiannon mukaan ja CodeGradesta läpi",
@@ -64,47 +66,59 @@ category_texts = [
                 "analyysien toteutus",
                 "toteutuksen selkeys, ymmärrettävä, ylläpidettävä ja laajennettava",
             ]
-error_list = []
 
-for i in range(0, 11):
-    error_list.append("import ei ole päätasolla, vaan sisennettynä aliohjelman/luokan sisään.")
-with dpg.group(tag=error_list_window, parent=center_window):
-    i = 0    
-    j= 0
-    for category in category_texts:      
-        dpg.add_collapsing_header(label=category, default_open=False, parent=error_list_window, tag="header_" + str(i))
+
+error_list, category_list = gui.initiate_problem_list()
+
+
+i = 0    
+j= 0
+
+with dpg.table(label="test", parent=center_window):
+    dpg.add_table_column(label="test")
+    dpg.add_table_column(label="test2")
+    for a in range (0,4):
+        
+        with dpg.table_row():
+            dpg.add_collapsing_header(label="test1")
+            for n in range(0,2):
+                dpg.add_button(label="Test")
+
+with dpg.table(label="testi", parent=center_window, tag="Table"):
+    dpg.add_table_column(label="Virheet")
+    dpg.add_table_column(label="Määrä")
+    for categ in category_list:
+       # with dpg.group(tag="group_"+str(i), width=50):
+        with dpg.table_row():
+            
+        
+            
+        
+            dpg.add_collapsing_header(label=category_texts[i], default_open=False, tag="header_" + str(i))
             #with dpg.tree_node(label= "Tree node 1", default_open=True, tag = "Tree_Node"):
         
-      
-            
-        with dpg.group(horizontal=True, parent="header_" + str(i)):
-            dpg.add_text(default_value=error_list[i])
-            dpg.add_spacer()
-            dpg.add_button(label="+", callback=gui.add_value, tag="button" + str(i))
-            dpg.add_spacer()
-            dpg.add_text(default_value="0", tag="value"+str(i))
-            dpg.add_spacer()
-            dpg.add_button(label="-")
+            for a in range(0, len(category_list)):
+                dpg.add_button(label="test")
+                for error in error_list:
+                
+                    if error.category == categ.category:
+                        with dpg.group(horizontal=True, parent="header_" + str(i), tag="error_"+ str(j)):
+                            dpg.add_text(default_value=error.text)
+                            dpg.add_spacer()
+                            dpg.add_button(label="+", callback=gui.add_value, tag="button" + str(j))
+                            dpg.add_spacer()
+                            dpg.add_text(default_value="0", tag="value"+str(j))
+                            dpg.add_spacer()
+                            dpg.add_button(label="-")
+                    j += 1
+            i += 1
+       
+
+# error_window_width = dpg.get_item_width("Virheet")
+# error_window_heigth = dpg.get_item_height("Virheet")
+# print(error_window_width, error_window_heigth)
+
              
-        i += 1
-    # with dpg.table(header_row=True):
-    #     dpg.add_table_column(label="Virheet")
-    #     dpg.add_table_column(label="Määrä")
-  
-
-    #     for i in range(0,4):
-    #         with dpg.table_row():
-    #             for j in range(0, 2):
-    #                 dpg.add_button(label="+")
-    #                 dpg.add_spacer()
-    #                 dpg.add_text(amount)
-    #                 dpg.add_spacer()
-    #                 dpg.add_button(label="-")
-                    
-
-
-    
-
 # with dpg.font_registry():
 #     with dpg.font("NotoSerif-Regular.ttf", 15) as font1:
 #         dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
@@ -120,7 +134,8 @@ dpg.show_item_registry()
 
 while dpg.is_dearpygui_running():
     dpg.render_dearpygui_frame()
+
     if (studentList != []):
-        dpg.configure_item("Test", items=studentList)
+        dpg.configure_item("Students", items=studentList)
 
 dpg.destroy_context()
--- GradingGUI_library.py
+++ GradingGUI_library.py
@@ -1,15 +1,17 @@
 import dearpygui.dearpygui as dpg
 import os, json
 
-class ErrorInfo:
+class ERRORINFO:
     error = ("",)
+    text = ""
     severity = 0
     amount = 0
     alternative = []
     exclude = []
     feedback = ""
+    category = ""
 
-class Category:
+class CATEGORY:
     category = ""
     category_sum = 0
     
@@ -39,29 +41,64 @@ def add_value(sender, app_data, user_data):
     
 
 def initiate_problem_list():
-    
     try:
         with open("Problem_list.json", "r", encoding="utf-8") as file:
-            errorsList = json.load(file) #loads json file as dict
+            ProblemList = json.load(file) #loads json file as dict
             file.close()
     except IOError as e:
         print("Error: ", e)
 
     
-    categ = errorsList["violations"][0]["category"]
-    c = Category()
-    c.category = categ
-    c.category_sum = 2
-
+    categ = ProblemList["violations"][0]["category"]
     categoryList = []
-    for i in errorsList["violations"]:
-        if (i["category"] not in categoryList):
-            categoryList.append(i["category"])
+    category_list = []
 
-    for i in categoryList:
-        print(i)
+    for i in ProblemList["violations"]:
+        if (i["category"] not in category_list):
+            category_list.append(i["category"])
+            c = CATEGORY()
+            c.category = i["category"]
+            c.category_sum = 0
+            categoryList.append(c)
+    ErrorList = []
     
-    for error in errorsList["violations"]:
+           
+            
         
-        if categ != error[""]:
-            pass
+
+    id = 0
+    errorID = 0
+    for section in ProblemList["violations"]:
+        if (categ != section["category"]):
+            errorID += 1
+            ErrorInfo = ERRORINFO()
+            ErrorInfo.error = section["ID"]
+            ErrorInfo.text = section["text"]
+            ErrorInfo.severity = section["error_values"] 
+            ErrorInfo.amount = section["error_values"].keys()
+            ErrorInfo.feedback = section["feedback"]
+            ErrorInfo.category = section["category"]
+            ErrorList.append(ErrorInfo)
+
+            
+        else:
+            ErrorInfo = ERRORINFO()
+            ErrorInfo.error = section["ID"]
+            ErrorInfo.severity = section["error_values"] 
+            ErrorInfo.amount = section["error_values"].keys()
+            ErrorInfo.feedback = section["feedback"]
+            ErrorList.append(ErrorInfo)
+
+    return ErrorList, categoryList
+
+# def add_error_to_group(id, text, id_number):
+#     print(id)
+#     with dpg.add_group(parent=id, horizontal=True, tag="group_" + str(id_number)):
+#         dpg.add_text(default_value=text)
+#         dpg.add_spacer()
+#         dpg.add_button(label="+")
+#         dpg.add_spacer()
+#         dpg.add_button(label="-")
+
+
+#     return 
